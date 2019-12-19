0179acc75261ee0549bca95eed758cf2ad947c36
Michael Olson <michaelolson.1996@protonmail.com>
2019-12-19 11:50:50 -0700

    added questions page

--- handclasp_site/components/questions/components/Question.js
+++ handclasp_site/components/questions/components/Question.js
@@ -1,15 +1,40 @@
+import { PureComponent } from 'react';
 
 const Question = (props) => {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         styles: {
+    //             display: 'none'
+    //         }
+    //     }
+    // }
 
-    const attr = props.value == "none" ? {style: {display: 'none'}} : {style: {display: 'visible'}}
+    // onClick = () => {
+    //     if (this.state.styles.display == 'none') {
+    //         this.setState(() => ({
+    //             styles: {
+    //                 display: 'visible',
+    //             }
+    //         }))
+    //     } else {
+    //         this.setState(() => ({
+    //             styles: {
+    //                 display: 'none'
+    //             }
+    //         }))    
+    //     }
+    // }
+
+
     return (
         <React.Fragment>
             <div className="question_answer_container">
-                <div onClick={() => props.onClick(props.index)} className ="question_container">
+                <div className ="question_container">
                     <h3 className="question">{props.question}</h3>
-                    <i className="fas fa-angle-right question_right_arrow"></i>
+                    {/* <i className="fas fa-angle-right question_right_arrow"></i> */}
                 </div>
-                <div className="answer_container" {...attr}>
+                <div className="answer_container">
                     <p className="answer">{props.answer}</p>
                 </div>
             </div>
